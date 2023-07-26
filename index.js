36746c78fc88392c0459fb339dde72a9b9d9ef37
Dipto Das <ddipto396@gmail.com>
2023-07-26 14:21:45 +0600

    Task-6 solution done

--- index.js
+++ index.js
@@ -87,21 +87,40 @@
 
 // Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
 
-function calculator(num1, operator, num2) {
-    switch (operator) {
-      case "+":
-        return num1 + num2;
-      case "-":
-        return num1 - num2;
-      case "*":
-        return num1 * num2;
-      case "/":
-        return num1 / num2;
-      default:
-        return "Invalid operator";
+// function calculator(num1, operator, num2) {
+//     switch (operator) {
+//       case "+":
+//         return num1 + num2;
+//       case "-":
+//         return num1 - num2;
+//       case "*":
+//         return num1 * num2;
+//       case "/":
+//         return num1 / num2;
+//       default:
+//         return "Invalid operator";
+//     }
+//   }
+  
+//   console.log(calculator(10, "+", 5));
+//   console.log(calculator(10, "*", 2));
+//   console.log(calculator(10, "/", 2));
+
+
+
+//   Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+function generateRandomPassword(length) {
+    const charset =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
     }
+    return password;
   }
   
-  console.log(calculator(10, "+", 5));
-  console.log(calculator(10, "*", 2));
-  console.log(calculator(10, "/", 2));
+  console.log(generateRandomPassword(12));
+  console.log(generateRandomPassword(10));
+  console.log(generateRandomPassword(8));
